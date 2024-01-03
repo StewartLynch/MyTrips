ca6f69bd9768b36d89a4361fc7eab9fba3a1e53a
Stewart Lynch <slynch@createchsol.com>
2024-01-03 10:58:07 -0800

    Completed Code for First Video

---
+++ MyTrips/SampleParisLocations.swift
@@ -0,0 +1,49 @@
+// Created for MyTrips
+// by  Stewart Lynch on 2024-01-01
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import CoreLocation
+
+// Sample Paris Location
+extension CLLocationCoordinate2D {
+    static let moulinRouge: Self = .init(
+        latitude: 48.884134,
+        longitude: 2.332196
+    )
+    
+    static let eiffelTower: Self = .init(
+        latitude: 48.858258,
+        longitude: 2.294488
+    )
+    
+    static let arcDeTriomphe: Self = .init(
+        latitude: 48.873776,
+        longitude: 2.295043
+    )
+    static let gareDuNord: Self = .init(
+        latitude: 48.880071,
+        longitude: 2.354977
+    )
+    static let louvre: Self = .init(
+        latitude: 48.861950,
+        longitude: 2.336902
+    )
+    static let sacreCoeur: Self = .init(
+        latitude: 48.886634,
+        longitude: 2.343048
+    )
+    static let notreDame: Self = .init(
+        latitude: 48.852972,
+        longitude: 2.350004
+    )
+    static let pantheon: Self = .init(
+        latitude: 48.845616,
+        longitude: 2.345996
+    )
+}
