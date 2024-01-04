135d4b5a1931a6320cccdb56e08f1def68815285
Stewart Lynch <slynch@createchsol.com>
2024-01-04 14:14:58 -0800

    Added LinkeIn to Header

--- MyTrips/StartTab.swift
+++ MyTrips/StartTab.swift
@@ -5,6 +5,7 @@
 // Follow me on Mastodon: @StewartLynch@iosdev.space
 // Follow me on Threads: @StewartLynch (https://www.threads.net)
 // Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
 // Subscribe on YouTube: https://youTube.com/@StewartLynch
 // Buy me a ko-fi:  https://ko-fi.com/StewartLynch
 
