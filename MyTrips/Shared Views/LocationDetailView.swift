cf7514ba05230ad030194491886781581185b94f
Stewart Lynch <slynch@createchsol.com>
2024-01-07 21:26:09 -0800

    V5 Work in progress

---
+++ MyTrips/Shared Views/LocationDetailView.swift
@@ -0,0 +1,25 @@
+//
+// Created for MyTrips
+// by  Stewart Lynch on 2024-01-07
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+import MapKit
+import SwiftData
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
